564192ff0f75a5d5c897b4f87a0387cbb3494922
Junayed5 <mdjunayed@gmail.com>
2023-06-22 23:27:42 +0600

    updating bio

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
+import { FaFacebookF } from "react-icons/fa";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { AiFillInstagram } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
 
 const Hero = () => {
     return (
@@ -12,22 +16,44 @@ const Hero = () => {
                         <h3 className='text-3xl font-bold mb-4'>
                             And I'm a <span></span>
                             <TypeAnimation
-                            sequence={[
-                                'Web developer',
-                                500,
-                                "Frontend Developer",
-                                1000,
-                            ]}
-                            className='text-cyan-300'
-                            repeat={Infinity}
+                                sequence={[
+                                    'Web developer',
+                                    500,
+                                    "Frontend Developer",
+                                    1000,
+                                ]}
+                                className='text-cyan-300'
+                                repeat={Infinity}
                             />
                         </h3>
                         <p className="text-lg font-semibold mb-6">
                             I'm a passionate web developer with expertise in front-end
                             development. Check out my projects and get in touch!
                         </p>
-                        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+
+                        <div className='flex gap-4 mb-5'>
+                            <a href="https://www.facebook.com/profile.php?id=100042861825143" target='_blank'>
+                                <FaFacebookF className=' text-cyan-300 h-fit w-fit border-2 px-2 py-2 rounded-full border-cyan-300 hover:bg-cyan-300 hover:text-black hover:shadow-cyan-300 hover:shadow-sm hover:transition hover:duration-500 hover:ease-in-out' />
+                            </a>
+                            <a href="https://www.linkedin.com/in/junayedmd/" target='_blank'>
+
+                                <BiLogoLinkedin className=' text-cyan-300 h-fit w-fit border-2 px-2 py-2 rounded-full border-cyan-300 hover:bg-cyan-300 hover:text-black hover:shadow-cyan-300 hover:shadow hover:transition hover:duration-500 hover:ease-in-out ' />
+                            </a>
+                            <a href="https://www.instagram.com/junayed740/" target='_blank'>
+
+                                <AiFillInstagram className=' text-cyan-300 h-fit w-fit border-2 px-2 py-2 rounded-full border-cyan-300 hover:bg-cyan-300 hover:text-black hover:shadow-cyan-300 hover:shadow hover:transition hover:duration-500 hover:ease-in-out ' />
+                            </a>
+                            <a href="https://github.com/Junayed5" target='_blank'>
+
+                                <BsGithub className=' text-cyan-300 h-fit w-fit border-2 px-2 py-2 rounded-full border-cyan-300 hover:bg-cyan-300 hover:text-black hover:shadow-cyan-300 hover:shadow hover:transition hover:duration-500 hover:ease-in-out ' />
+                            </a>
+                        </div>
+
+                        {/* <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
                             View Projects
+                        </button> */}
+                        <button className="h-12 w-44 bg-cyan-300 rounded-full shadow-sm shadow-cyan-300 hover:shadow-none text-slate-800 font-bold">
+                            <a href="#" className=''>Download Resume</a>
                         </button>
                     </div>
                     <div className="md:col-span-1 px-4">
