9d3f68aad58e0753bfabb3d7940a567bacb9a9a3
Junayed5 <mdjunayed@gmail.com>
2023-06-25 06:03:30 +0600

    updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,11 +6,6 @@ const AboutMe = () => {
             <div className="container mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2">
                     <div className="md:col-span-1 px-12">
-                        {/* <img
-                            src="https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60"
-                            alt="Portfolio Image"
-                            className="w-full rounded"
-                        /> */}
                         <div className="h-48 w-48">
                             <img className='w-fit' src="me.jpg" alt="" />
                         </div>
@@ -19,10 +14,10 @@ const AboutMe = () => {
                     <div className="md:col-span-1 px-4">
                         <h1 className="text-5xl font-bold mb-4">About <span className='text-cyan-300'>Me</span></h1>
                         <h3 className='text-3xl font-bold mb-4'>
-                            Frontend Developer
+                            Frontend Developer!
                         </h3>
 
-                        <p className='mb-4 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam inventore eum veritatis sit corporis nulla dolore suscipit quas. Laborum, repellendus libero vero sit esse enim, ullam consequatur explicabo pariatur dignissimos optio voluptatum est architecto? Omnis ducimus exercitationem laboriosam repellat. Delectus ex velit neque modi voluptate, perferendis voluptatem sunt ut ipsam.</p>
+                        <p className='mb-4 font-medium '>Hello! I am Junayed, a passionate Frontend Developer. My core skill is based on Javascript. I developed more web application. I create more application Javascript popular library React Js. I love to make the web more open to the world. I am non CSE person but I learn it on my passionate place. I am available for any kind of job opportunity that suits my interests.</p>
                         
                         <button className="h-12 w-36 bg-cyan-300 rounded-full shadow-sm shadow-cyan-300 hover:shadow-none text-slate-800 font-bold">
                             <a href="#" className=''>Read More</a>
