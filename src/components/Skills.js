993751c7c7456cf267f8d6eb17c73aeda18c49d8
Junayed5 <mdjunayed@gmail.com>
2023-07-05 09:33:24 +0600

    skills section updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,76 +6,134 @@ const Skills = () => {
             <div className="container mx-auto">
                 <h3 className='text-5xl font-bold text-center pb-10'>My <span className='text-cyan-300'>Skills</span></h3>
 
-                <div className=''>
+                <h3 className='text-3xl font-bold text-center pb-3'><span className=''>Coding</span>  Skill </h3>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-x-52 w-fit mx-auto'>
                     <div>
-                        <div className='w-96 flex justify-between px-1 py-2'>
-                            <p className='font-semibold'>HTML</p>
-                            <p className='font-semibold'>90%</p>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 py-2'>
+                                <p className='font-semibold'>HTML</p>
+                                <p className='font-semibold'>90%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-80 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
-                        <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
-                            <span className='absolute bg-cyan-300 h-3 w-80 rounded-full shadow-sm shadow-cyan-300'></span>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>CSS</p>
+                                <p className='font-semibold'>75%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-72 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>Bootstrap</p>
+                                <p className='font-semibold'>70%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-72 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>Tailwind CSS</p>
+                                <p className='font-semibold'>70%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-72 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>Github</p>
+                                <p className='font-semibold'>45%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-48 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>JavaScript</p>
+                                <p className='font-semibold'>50%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-52 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>React JS</p>
+                                <p className='font-semibold'>40%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-36 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
                     </div>
                     <div>
-                        <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
-                            <p className='font-semibold'>CSS</p>
-                            <p className='font-semibold'>75%</p>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-2'>
+                                <p className='font-semibold'>Node Js</p>
+                                <p className='font-semibold'>20%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-20 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
-                        <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
-                            <span className='absolute bg-cyan-300 h-3 w-72 rounded-full shadow-sm shadow-cyan-300'></span>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>Express JS</p>
+                                <p className='font-semibold'>25%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-24 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
-                            <p className='font-semibold'>Github</p>
-                            <p className='font-semibold'>45%</p>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>MongoDB</p>
+                                <p className='font-semibold'>25%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-24 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
-                        <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
-                            <span className='absolute bg-cyan-300 h-3 w-48 rounded-full shadow-sm shadow-cyan-300'></span>
+
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>Next Js</p>
+                                <p className='font-semibold'>30%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-24 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
-                            <p className='font-semibold'>JavaScript</p>
-                            <p className='font-semibold'>50%</p>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>Redux</p>
+                                <p className='font-semibold'>30%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-24 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
-                        <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
-                            <span className='absolute bg-cyan-300 h-3 w-52 rounded-full shadow-sm shadow-cyan-300'></span>
-                        </div>
-                    </div>
-                    <div>
-                        <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
-                            <p className='font-semibold'>React JS</p>
-                            <p className='font-semibold'>40%</p>
-                        </div>
-                        <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
-                            <span className='absolute bg-cyan-300 h-3 w-36 rounded-full shadow-sm shadow-cyan-300'></span>
-                        </div>
-                    </div>
-                    <div>
-                        <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
-                            <p className='font-semibold'>Express JS</p>
-                            <p className='font-semibold'>25%</p>
-                        </div>
-                        <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
-                            <span className='absolute bg-cyan-300 h-3 w-24 rounded-full shadow-sm shadow-cyan-300'></span>
-                        </div>
-                    </div>
-                    <div>
-                        <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
-                            <p className='font-semibold'>MongoDB</p>
-                            <p className='font-semibold'>25%</p>
-                        </div>
-                        <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
-                            <span className='absolute bg-cyan-300 h-3 w-24 rounded-full shadow-sm shadow-cyan-300'></span>
+                        <div>
+                            <div className='w-96 flex justify-between px-1 pb-2 pt-5'>
+                                <p className='font-semibold'>Metarial UI</p>
+                                <p className='font-semibold'>25%</p>
+                            </div>
+                            <div className='h-3 w-96 bg-gray-900 rounded-full relative'>
+                                <span className='absolute bg-cyan-300 h-3 w-24 rounded-full shadow-sm shadow-cyan-300'></span>
+                            </div>
                         </div>
                     </div>
                 </div>
-                {/* <div className='py-10'>
-                    <div className='h-20 w-20 border-8 border-cyan-300 rounded-full border-'>
 
-                    </div>
-                </div> */}
+                <h3 className='text-3xl font-bold text-center pt-8 pb-3'><span className=''>Professional</span>  Skill </h3>
             </div>
         </div>
     );
