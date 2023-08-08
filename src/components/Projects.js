29946e45fcb5ffcbbcc0dd035aa415e4696f4892
Junayed5 <mdjunayed@gmail.com>
2023-08-08 12:40:30 +0600

    project section almost complete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../App.css'
 import { HiOutlineExternalLink } from 'react-icons/hi'
 
 const Projects = () => {
@@ -8,9 +9,9 @@ const Projects = () => {
                 <h3 className='text-5xl font-bold text-center pb-10'>Latest <span className='text-cyan-300'>Projects</span></h3>
 
                 <div className='grid grid-cols-3 align-middle gap-7'>
-                    <div className='relative rounded-lg shadow-cyan-300 overflow-hidden flex '>
+                    <div className='relative rounded-lg shadow-cyan-300 overflow-hidden flex project-detail'>
                         <img className='w-full ease-out duration-500 hover:scale-110' src="https://i.ibb.co/4Pmyj3r/project.png" alt="" />
-                        <div className='absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-cyan-300 to-[rgba(0,0,0,.1)] flex justify-center align-middle flex-col text-center px-3 translate-y-full ease-out duration-500 hover:translate-y-0'>
+                        <div className='absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-cyan-300 to-[rgba(0,0,0,.1)] flex justify-center align-middle flex-col text-center px-3 translate-y-full ease-out duration-500 hover:translate-y-0 detail-container'>
                             <h3 className='font-bold text-2xl'>Web design</h3>
                             <p className='text-xl mx-5 my-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, fugit.</p>
                             <a className='w-full flex justify-center' href="#">
@@ -20,9 +21,9 @@ const Projects = () => {
                             </a>
                         </div>
                     </div>
-                    <div className='relative rounded-lg shadow-cyan-300 overflow-hidden flex'>
+                    <div className='relative rounded-lg shadow-cyan-300 overflow-hidden flex project-detail'>
                         <img className='w-full ease-out duration-500 hover:scale-110' src="https://i.ibb.co/4Pmyj3r/project.png" alt="" />
-                        <div className='absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-cyan-300 to-[rgba(0,0,0,.1)] flex justify-center align-middle flex-col text-center px-3 translate-y-full ease-out duration-500 hover:translate-y-0'>
+                        <div className='absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-cyan-300 to-[rgba(0,0,0,.1)] flex justify-center align-middle flex-col text-center px-3 translate-y-full ease-out duration-500 hover:translate-y-0 detail-container'>
                             <h3 className='font-bold text-2xl'>Web design</h3>
                             <p className='text-xl mx-5 my-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, fugit.</p>
                             <a className='w-full flex justify-center' href="#">
@@ -32,9 +33,9 @@ const Projects = () => {
                             </a>
                         </div>
                     </div>
-                    <div className='relative rounded-lg shadow-cyan-300 overflow-hidden flex  hover:translate-y-0'>
+                    <div className='relative rounded-lg shadow-cyan-300 overflow-hidden flex  project-detail'>
                         <img className='w-full ease-out duration-500 hover:scale-110 ' src="https://i.ibb.co/4Pmyj3r/project.png" alt="" />
-                        <div className='absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-cyan-300 to-[rgba(0,0,0,.1)] flex justify-center align-middle flex-col text-center px-3 translate-y-full ease-out duration-500 hover:translate-y-0'>
+                        <div className='absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-cyan-300 to-[rgba(0,0,0,.1)] flex justify-center align-middle flex-col text-center px-3 translate-y-full ease-out duration-500 detail-container'>
                             <h3 className='font-bold text-2xl'>Web design</h3>
                             <p className='text-xl mx-5 my-2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, fugit.</p>
                             <a className='w-full flex justify-center' href="#">
